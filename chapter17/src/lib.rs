e98d603d6a1a0b02b46ff353082327b3184b1b70
apktool <apktoolOo@gmail.com>
2024-01-24 22:14:41 +0800

    [ADD] Oop

---
+++ chapter17/src/lib.rs
@@ -0,0 +1,40 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Button::draw()");
+    }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("SelectBox::draw()");
+    }
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
