81126840466c54e13246f1ce078505c0c735cf01
apktool <apktoolOo@gmail.com>
2024-01-25 23:52:48 +0800

    [ADD] web server with single threaded

---
+++ chapter20/src/main.rs
@@ -0,0 +1,35 @@
+use std::fmt::format;
+use std::fs;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3001").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let buffer = BufReader::new(&mut stream);
+        let request_line = buffer.lines().next().unwrap().unwrap();
+        if request_line == "GET / HTTP/1.1" {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = fs::read_to_string("hello.html").unwrap();
+            let length = contents.len();
+            let response =
+                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        } else if request_line == "POST / HTTP/1.1" {
+            let status_line = "HTTP/1.1 200 OK";
+            let contents = "{\"message\": \"OK\"}";
+            let length = contents.len();
+            let response =
+                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    }
+}
