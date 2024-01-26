481ea7f6a7273056e9959e8556a6a9294ca80a19
apktool <apktoolOo@gmail.com>
2024-01-26 18:03:04 +0800

    [ADD] web server with multi threaded

--- chapter20/src/main.rs
+++ chapter20/src/main.rs
@@ -1,34 +1,52 @@
 use std::fs;
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+use chapter20::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3001").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 
-    fn handle_connection(mut stream: TcpStream) {
-        let buffer = BufReader::new(&mut stream);
-        let request_line = buffer.lines().next().unwrap().unwrap();
-        if request_line == "GET / HTTP/1.1" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = fs::read_to_string("hello.html").unwrap();
-            let length = contents.len();
-            let response =
-                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    println!("Shutting down.");
+}
 
-            stream.write_all(response.as_bytes()).unwrap();
-        } else if request_line == "POST / HTTP/1.1" {
-            let status_line = "HTTP/1.1 200 OK";
-            let contents = "{\"message\": \"OK\"}";
-            let length = contents.len();
-            let response =
-                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-            stream.write_all(response.as_bytes()).unwrap();
-        }
-    }
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
